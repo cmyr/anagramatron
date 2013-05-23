9480189e283906591a9afe2aa99a940b4193c324
cmyr <colin@cmyr.net>
2013-05-23 17:02:07 -0400

    added cherypy config, got ssl working on the server

--- hit_server.py
+++ hit_server.py
@@ -1,9 +1,43 @@
-from bottle import route, run, request, response
+from __future__ import print_function
+from bottle import Bottle, route, run, request, response, server_names, ServerAdapter
 import datahandler
 
-data = datahandler.DataHandler(just_the_hits=True)
-HITS = data.get_all_hits()
+# SSL subclass of bottle cribbed from:
+# http://dgtool.blogspot.com.au/2011/12/ssl-encryption-in-python-bottle.html
 
+# Declaration of new class that inherits from ServerAdapter
+# It's almost equal to the supported cherrypy class CherryPyServer
+
+
+class MySSLCherryPy(ServerAdapter):
+    def run(self, handler):
+        import cherrypy
+        from cherrypy import wsgiserver
+        cherrypy.config.update('cherrypy.config')
+        # print(cherrypy.config.items())
+        server = cherrypy.wsgiserver.CherryPyWSGIServer(
+                                                        (self.host, self.port),
+                                                        handler,
+                                                        numthreads=1,
+                                                        max=1)
+        # print(server.requests._threads)
+        # If cert variable is has a valid path, SSL will be used
+        # You can set it to None to disable SSL
+        cert = 'data/server.pem'  # certificate path
+        server.ssl_certificate = cert
+        server.ssl_private_key = cert
+        try:
+            server.start()
+        finally:
+            server.stop()
+
+# Add our new MySSLCherryPy class to the supported servers
+# under the key 'mysslcherrypy'
+server_names['sslbottle'] = MySSLCherryPy
+
+# data = datahandler.DataHandler(just_the_hits=True)
+# HITS = data.get_all_hits()
+data = None
 
 def hit_for_id(hit_id):
     for hit in HITS:
@@ -13,11 +47,14 @@ def hit_for_id(hit_id):
 
 @route('/hits')
 def get_hits():
-    HITS = data.get_all_hits()
-    return {'hits': HITS}
+    global data
+    if not data:
+        data = datahandler.DataHandler(just_the_hits=True)
+    hits = data.get_all_hits()
+    return {'hits': hits}
 
 
-@route('/rt')
+@route('/ok')
 def retweet():
     hit_id = int(request.query.id)
     hit = hit_for_id(hit_id)
@@ -32,7 +69,7 @@ def delete():
     return "success"
 
 
-run(host='localhost', port=8080, debug=True)
+run(host='localhost', port=8080, debug=True, server='sslbottle')
 
 # if __name__ == "__main__":
 #     print hit_for_id(1368809545607)
