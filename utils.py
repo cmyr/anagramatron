70ad818158032ca0c15aa21197bf8a03bc73dd8a
cmyr <colin@cmyr.net>
2013-05-23 13:54:14 -0400

    made format_seconds less embarassing to look at

--- utils.py
+++ utils.py
@@ -1,46 +1,20 @@
 import re
 
+
 def format_seconds(seconds):
     """
-    yea fine this is bad deal with it
+    convert a number of seconds into a custom string representation
     """
-    seconds = int(seconds)
-    DAYSECS = 86400
-    HOURSECS = 3600
-    MINSECS = 60
-    dd = hh = mm = ss = 0
-
-    dd = seconds / DAYSECS
-    seconds = seconds % DAYSECS
-    hh = seconds / HOURSECS
-    seconds = seconds % HOURSECS
-    mm = seconds / MINSECS
-    seconds = seconds % MINSECS
-    ss = seconds
-    # time_string = (str(mm)+'m ' + str(ss) + 's')
-    time_string = ("%im %s" %(mm,ss))
-    if hh or dd:
-        # time_string = str(hh) + 'h ' + time_string
-        time_string = "%ih %s" % (hh, time_string)
-    if dd:
-        # time_string = str(dd) + 'd ' + time_string
-        time_string = "%id %s" % (dd, time_string)
+    d, seconds = divmod(seconds, (60*60*24))
+    h, seconds = divmod(seconds, (60*60))
+    m, seconds = divmod(seconds, 60)
+    time_string = ("%im %0.2fs" % (m, seconds))
+    if h or d:
+        time_string = "%ih %s" % (h, time_string)
+    if d:
+        time_string = "%id %s" % (d, time_string)
     return time_string
 
-
-if __name__ == "__main__":
-    tests = [10,
-             12.025808095932007,
-             15.282587051391602,
-             101,
-             1124,
-             23232,
-             4334321,
-             1231450698173748]
-
-    for t in tests:
-        print format_seconds(t)
-        print "format test: %s" % format_seconds(t)
 
 def show_anagram(one, two):
     print one
@@ -59,3 +33,6 @@ def stripped_string(text, spaces=False):
     if spaces:
         return re.sub(r'[^a-zA-Z]', ' ', text).lower()
     return re.sub(r'[^a-zA-Z]', '', text).lower()
+
+if __name__ == "__main__":
+    pass
