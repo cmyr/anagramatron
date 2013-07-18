53caa30bd24dcc299fea0c8af782f6ce1878cfeb
cmyr <colin@cmyr.net>
2013-07-18 18:31:49 -0400

    adding back untracked stuff

---
+++ anagramfunctions.py
@@ -0,0 +1,263 @@
+import re
+import anagramstats as stats
+import sqlite3 as lite
+import unicodedata
+
+from constants import (ANAGRAM_LOW_CHAR_CUTOFF, ANAGRAM_LOW_UNIQUE_CHAR_CUTOFF,
+                       ENGLISH_LETTER_FREQUENCIES)
+
+ENGLISH_LETTER_LIST = sorted(ENGLISH_LETTER_FREQUENCIES.keys(),
+                             key=lambda t: ENGLISH_LETTER_FREQUENCIES[t])
+# This contains the various functions for filtering tweets, comparing
+# potential anagrams, as well as some shared helper utilities.
+def old_filter(tweet):
+    """
+    this is here for some legacy testing
+    """
+    #check for mentions
+    if len(tweet.get('entities').get('user_mentions')) is not 0:
+        return False
+    #check for retweets
+    if tweet.get('retweeted_status'):
+        return False
+    # ignore tweets w/ non-ascii characters
+    try:
+        tweet['text'].decode('ascii')
+    except UnicodeEncodeError:
+        return False
+    # check for links:
+    if len(tweet.get('entities').get('urls')) is not 0:
+        return False
+    # ignore short tweets
+    t = utils.stripped_string(tweet['text'])
+    if len(t) <= ANAGRAM_LOW_CHAR_CUTOFF:
+        return False
+    # ignore tweets with few characters
+    st = set(t)
+    if len(st) <= ANAGRAM_LOW_UNIQUE_CHAR_CUTOFF:
+        return False
+    return format_tweet(tweet)
+
+
+freqsort = ENGLISH_LETTER_FREQUENCIES
+# just to keep line_lengths sane
+
+def improved_hash(text, debug=False):
+    """
+    only very *minorly* improved. sorts based on letter frequencies.
+    """
+    CHR_COUNT_START = 64  # we convert to chars; char 65 is A
+    t_text = stripped_string(text)
+    t_hash = ''.join(sorted(t_text, key=lambda t: freqsort[t]))
+    letset = set(t_hash)
+    break_letter = t_hash[-1:]
+    if break_letter not in ENGLISH_LETTER_LIST:
+        break_letter = ENGLISH_LETTER_LIST[-1]
+    compressed_hash = ''
+    for letter in ENGLISH_LETTER_LIST:
+        if letter in letset:
+            count = len(re.findall(letter, t_hash))
+            count = (count if count < 48 else 48)
+            # this is a hacky way of sanity checking our values.
+            # if this shows up as a match we'll ignore it
+            compressed_hash += chr(count + CHR_COUNT_START)
+        else:
+            if freqsort[letter] > freqsort[break_letter]:
+                if len(compressed_hash) % 2:
+                    # an uneven number of bytes will cause unicode errors
+                    compressed_hash += chr(64)
+                break
+            compressed_hash += chr(64)
+
+    if len(compressed_hash) == 0:
+        print('hash length is zero?')
+        return '@@'
+    return compressed_hash
+    # return t_hash
+
+
+def _correct_encodings(text):
+    """
+    twitter auto converts &, <, > to &amp; &lt; &gt;
+    """
+    text = re.sub(r'&amp;', '&', text)
+    text = re.sub(r'&lt;', '<', text)
+    text = re.sub(r'&gt;', '>', text)
+    return text
+
+
+def _strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
+
+
+def _text_contains_tricky_chars(text):
+    if re.search(ur'[\u0080-\u024F]', text):
+        return True
+    return False
+
+
+def _text_decodes_to_ascii(text):
+    try:
+        text.decode('ascii')
+    except UnicodeEncodeError:
+        return False
+    return True
+
+
+def _basic_filters(tweet):
+    if tweet.get('lang') != 'en':
+        return False
+    if len(tweet.get('entities').get('user_mentions')) is not 0:
+        return False
+    #check for retweets
+    if tweet.get('retweeted_status'):
+        return False
+    # check for links:
+    if len(tweet.get('entities').get('urls')) is not 0:
+        return False
+    t = stripped_string(tweet['text'])
+    if len(t) <= ANAGRAM_LOW_CHAR_CUTOFF:
+        return False
+    # ignore tweets with few characters
+    st = set(t)
+    if len(st) <= ANAGRAM_LOW_UNIQUE_CHAR_CUTOFF:
+        return False
+    return True
+
+
+def _low_letter_ratio(text, cutoff=0.8):
+    t = re.sub(r'[^a-zA-Z .,!?"\']', '', text)
+    if (float(len(t)) / len(text)) < cutoff:
+        return True
+    return False
+
+
+# def print_low_letter_ratio(text, cutoff=0.8):
+#     t = re.sub(r'[^a-zA-Z .,!?"\']', '', text)
+#     return (float(len(t)) / len(text))
+
+
+def filter_tweet(tweet):
+    """
+    filters out anagram-inappropriate tweets.
+    Returns the original tweet object and cleaned tweet text on success.
+    """
+    if not _basic_filters(tweet):
+        return False
+
+    tweet_text = _correct_encodings(tweet.get('text'))
+    if not _text_decodes_to_ascii(tweet_text):
+        # check for latin chars:
+        if _text_contains_tricky_chars(tweet_text):
+            tweet_text = _strip_accents(tweet_text)
+
+    if _low_letter_ratio(tweet_text):
+        return False
+
+    return {'tweet_hash': improved_hash(tweet_text),
+            'tweet_id': long(tweet['id_str']),
+            'tweet_text': tweet_text
+            }
+
+
+
+def test_anagram(string_one, string_two):
+    """
+    most basic test, finds if tweets are just identical
+    """
+    stats.possible_hit()
+    if not _char_diff_test(string_one, string_two):
+        return False
+    if not _word_diff_test(string_one, string_two):
+        return False
+
+    stats.hit()
+    return True
+
+
+def _char_diff_test(string_one, string_two, cutoff=0.5):
+    """
+    basic test, looks for similarity on a char by char basis
+    """
+    stripped_one = stripped_string(string_one)
+    stripped_two = stripped_string(string_two)
+
+    total_chars = len(stripped_two)
+    same_chars = 0
+
+    if len(stripped_one) != len(stripped_two):
+        print('diff check called on unequal length strings')
+        print(string_one, string_two)
+        return False
+
+    for i in range(total_chars):
+        if stripped_one[i] == stripped_two[i]:
+            same_chars += 1
+    try:
+        if (float(same_chars) / total_chars) < cutoff:
+            return True
+    except ZeroDivisionError:
+        print(string_one, string_two)
+    return False
+
+
+def _word_diff_test(string_one, string_two, cutoff=0.5):
+    """
+    looks for tweets containing the same words in different orders
+    """
+    words_one = stripped_string(string_one, spaces=True).split()
+    words_two = stripped_string(string_two, spaces=True).split()
+
+    word_count = len(words_one)
+    same_words = 0
+
+    if len(words_two) < len(words_one):
+            word_count = len(words_two)
+        # compare words to each other:
+    for word in words_one:
+        if word in words_two:
+            same_words += 1
+        # if more then $CUTOFF words are the same, fail test
+    if (float(same_words) / word_count) < cutoff:
+        return True
+    else:
+        return False
+
+
+def format_seconds(seconds):
+    """
+    convert a number of seconds into a custom string representation
+    """
+    d, seconds = divmod(seconds, (60*60*24))
+    h, seconds = divmod(seconds, (60*60))
+    m, seconds = divmod(seconds, 60)
+    time_string = ("%im %0.2fs" % (m, seconds))
+    if h or d:
+        time_string = "%ih %s" % (h, time_string)
+    if d:
+        time_string = "%id %s" % (d, time_string)
+    return time_string
+
+
+def show_anagram(one, two):
+    print one
+    print two
+    print stripped_string(one, spaces=True)
+    print stripped_string(two, spaces=True)
+    print stripped_string(one)
+    print stripped_string(two)
+    print ''.join(sorted(stripped_string(two), key=str.lower))
+
+
+def stripped_string(text, spaces=False):
+    """
+    returns lower case string with all non alpha chars removed
+    """
+    if spaces:
+        return re.sub(r'[^a-zA-Z]', ' ', text).lower()
+    return re.sub(r'[^a-zA-Z]', '', text).lower()
+
+
+if __name__ == "__main__":
+    pass
---
+++ hitmanager.py
@@ -0,0 +1,219 @@
+import sqlite3 as lite
+import os
+import time
+
+import anagramconfig
+import anagramfunctions
+import logging
+
+from twitterhandler import TwitterHandler
+HIT_PATH_COMPONENT = 'hitdata'
+
+HIT_STATUS_REVIEW = 'review'
+HIT_STATUS_REJECTED = 'rejected'
+HIT_STATUS_POSTED = 'posted'
+HIT_STATUS_APPROVED = 'approved'
+HIT_STATUS_MISC = 'misc'
+HIT_STATUS_FAILED = 'failed'
+
+dbpath = None
+hitsdb = None
+twitter_handler = None
+LOG_FILE_NAME = 'data/hitman.log'
+
+def _setup(languages=['en']):
+    logging.basicConfig(
+        filename=LOG_FILE_NAME,
+        format='%(asctime)s - %(levelname)s:%(message)s',
+        level=logging.DEBUG
+    )
+
+    global dbpath, hitsdb
+    dbpath = (anagramconfig.STORAGE_DIRECTORY_PATH +
+              HIT_PATH_COMPONENT +
+              '_'.join(languages) + '.db')
+
+    if not os.path.exists(dbpath):
+        hitsdb = lite.connect(dbpath)
+        cursor = hitsdb.cursor()
+        print('hits db not found, creating')
+        cursor.execute("""CREATE TABLE hits
+            (hit_id INTEGER, hit_status TEXT, hit_date INTEGER, hit_hash TEXT, hit_rating text, flags TEXT, one_id text, two_id text, one_text text, two_text text)""")
+        cursor.execute("CREATE TABLE blacklist (bad_hash TEXT UNIQUE)")
+        hitsdb.commit()
+    else:
+        hitsdb = lite.connect(dbpath)
+
+
+def _checkit():
+    if not dbpath or hitsdb:
+        _setup()
+
+
+def new_hit(first, second):
+    _checkit()
+    hit = {
+           "id": int(time.time()*1000),
+           "status": HIT_STATUS_REVIEW,
+           "hash": first['tweet_hash'],
+           "tweet_one": first,
+           "tweet_two": second
+        }
+
+    if _hit_on_blacklist(hit):
+        return
+    if _hit_collides_with_previous_hit(hit):
+        return
+
+    _add_hit(hit)
+
+def _hit_on_blacklist(hit):
+    cursor = hitsdb.cursor()
+    cursor.execute("SELECT count(*) FROM blacklist WHERE bad_hash=?", (hit['hash'],))
+    result = cursor.fetchone()[0]
+    if result == 1:
+        logging.debug('hit on blacklist: %s' % hit['tweet_one']['tweet_text'])
+        return True
+    return False
+
+def _hit_collides_with_previous_hit(hit):
+    cursor = hitsdb.cursor()
+    cursor.execute("SELECT * FROM hits WHERE hit_hash=?",(hit['hash'],))
+    result = cursor.fetchone()
+    if result:
+        # do some comparisons
+        result = hit_from_sql(result)
+        r1 = result['tweet_one']['tweet_text']
+        r2 = result['tweet_two']['tweet_text']
+        t1 = hit['tweet_one']['tweet_text']
+        t2 = hit['tweet_two']['tweet_text']
+        if anagramfunctions.test_anagram(r1, t1):
+            # print('hit collision:', hit, result)
+            logging.debug('hit collision: %s %s' % (r1, t1))
+            return True
+        if anagramfunctions.test_anagram(r1, t2):
+            # print('hit collision:', hit, result)
+            logging.debug('hit collision: %s %s' % (r1, t2))
+            return True
+        if anagramfunctions.test_anagram(r2, t1):
+            # print('hit collision:', hit, result)
+            logging.debug('hit collision: %s %s' % (r2, t1))
+            return True
+        if anagramfunctions.test_anagram(r2, t2):
+            # print('hit collision:', hit, result)
+            logging.debug('hit collision: %s %s' % (r2, t2))
+            return True
+
+    return False
+
+def _add_hit(hit):
+    cursor = hitsdb.cursor()
+
+    cursor.execute("INSERT INTO hits VALUES (?,?,?,?,?,?,?,?,?,?)",
+                  (str(hit['id']),
+                   hit['status'],
+                   str(time.time()),
+                   str(hit['hash']),
+                   '0',
+                   '0',
+                   str(hit['tweet_one']['tweet_id']),
+                   str(hit['tweet_two']['tweet_id']),
+                   hit['tweet_one']['tweet_text'],
+                   hit['tweet_two']['tweet_text'])
+                   )
+    hitsdb.commit()
+
+
+def get_hit(hit_id):
+    _checkit()
+    cursor = hitsdb.cursor()
+    cursor.execute("SELECT * FROM hits WHERE hit_id=:id",
+                   {"id": str(hit_id)})
+    result = cursor.fetchone()
+    return hit_from_sql(result)
+
+
+def remove_hit(hit_id):
+    _checkit()
+    cursor = hitsdb.cursor()
+    cursor.execute("DELETE FROM hits WHERE hit_id=:id",
+                   {"id": str(hit_id)})
+    hitsdb.commit()
+
+
+def set_hit_status(hit_id, status):
+    _checkit()
+    if status not in [HIT_STATUS_REVIEW, HIT_STATUS_MISC,
+                      HIT_STATUS_APPROVED, HIT_STATUS_POSTED,
+                      HIT_STATUS_REJECTED, HIT_STATUS_FAILED]:
+        return False
+    # get the hit, delete the hit, add it again with new status.
+    hit = get_hit(hit_id)
+    hit['status'] = status
+    remove_hit(hit_id)
+    _add_hit(hit)
+
+
+def all_hits(with_status=None):
+    _checkit()
+    cursor = hitsdb.cursor()
+    if not filter:
+        cursor.execute("SELECT * FROM hits")
+    else:
+        cursor.execute("SELECT * FROM hits WHERE hit_status = (?)", (with_status,))
+    results = cursor.fetchall()
+    hits = []
+    for item in results:
+        hits.append(hit_from_sql(item))
+    return hits
+
+
+def blacklist():
+    cursor = hitsdb.cursor()
+    cursor.execute("SELECT * from blacklist")
+    results = cursor.fetchall()
+    return results
+
+
+def hit_from_sql(item):
+    """
+    convenience method for converting the result of an sql query
+    into a python dictionary compatable with anagramer
+    """
+    return {'id': long(item[0]),
+            'status': str(item[1]),
+            'timestamp': item[2],
+            'hash': str(item[3]),
+            'rating': str(item[4]),
+            'flags': str(item[5]),
+            'tweet_one': {'tweet_id': long(item[6]), 'tweet_text': item[8]},
+            'tweet_two': {'tweet_id': long(item[7]), 'tweet_text': item[9]}
+            }
+
+
+def add_to_blacklist(bad_hash):
+    _checkit()
+    cursor = hitsdb.cursor()
+    cursor.execute("INSERT OR IGNORE INTO blacklist VALUES (?)", (bad_hash,))
+    hitsdb.commit()
+
+
+def reject_hit(hit_id):
+    set_hit_status(hit_id, HIT_STATUS_REJECTED)
+    return True
+
+
+def post_hit(hit_id):
+    global twitter_handler
+    if not twitter_handler:
+        twitter_handler = TwitterHandler()
+    if twitter_handler.post_hit(get_hit(hit_id)):
+        set_hit_status(hit_id, HIT_STATUS_POSTED)
+        return True
+    else:
+        set_hit_status(hit_id, HIT_STATUS_FAILED)
+        return False
+
+def approve_hit(hit_id):
+    set_hit_status(hit_id, HIT_STATUS_APPROVED)
+    return True
