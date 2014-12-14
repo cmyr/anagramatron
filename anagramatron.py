cd150f07c0d89df26028d892454ccc79f8c23fd3
cmyr <colin@cmyr.net>
2014-12-14 18:58:17 -0500

    maybe I can't catch all exceptions :-(

--- anagramatron.py
+++ anagramatron.py
@@ -57,12 +57,12 @@ def run(server_only=False):
                 data_coordinator.close()
                 return 0
 
-            except Exception as err:
-                logging.error(err)
-                stream_handler.close()
-                data_coordinator.close()
-                TwitterHandler().send_message(str(err))
-                raise err
+            # except Exception as err:
+            #     logging.error(err)
+            #     stream_handler.close()
+            #     data_coordinator.close()
+            #     TwitterHandler().send_message(str(err))
+            #     raise err
 
 
 def main():
