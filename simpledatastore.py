64a64e0b4afe631b13e9d320aec2a49e6d6c5beb
cmyr <colin@cmyr.net>
2014-12-26 17:58:09 -0500

    added magic funcs & moved over load + save

--- simpledatastore.py
+++ simpledatastore.py
@@ -2,7 +2,8 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 
-
+ITEM_KEY = 'tweet'
+COUNT_KEY = 'hit_count'
 
 class AnagramSimpleStore(object):
     """AnagramSimpleStore is a simple data store implemented
@@ -11,8 +12,54 @@ class AnagramSimpleStore(object):
     def __init__(self, path):
         super(AnagramSimpleStore, self).__init__()
         self.path = path
-        self.datastore = dict()
+        self.datastore = self.load()
+    
+    def __len__(self):
+        return len(self.datastore)
+
+    def __contains__(self, item):
+        return self.datastore.get(item) != None
+
+    def __getitem__(self, key):
+        return self.datastore[key][ITEM_KEY]
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.datastore[key][ITEM_KEY] = value
+            self.datastore[key][COUNT_KEY] += 1
+        else:
+            self.datastore[key] = {ITEM_KEY: value, COUNT_KEY: 0}
+
+    def __delete__(self, instance):
+        del self.datastore[x]
         
+    def load(self):
+        print('loading cache')
+        cache = dict()
+        try:
+            loaded = pickle.load(open(self.path, 'r'))
+            for t in loaded:
+                cache[t['tweet_hash']] = {'tweet': t, 'hit_count': 0}
+            print('loaded %i items to cache' % len(cache))
+            return cache
+        except IOError:
+            logging.error('error loading cache :(')
+            return cache
+            # really not tons we can do ehre
+
+    def save(self):
+        """
+        pickles the data currently in the cache.
+        doesn't save hit_count. we don't want to keep briefly popular
+        items in cache indefinitely
+        """
+        to_save = [self.datastore[t]['tweet'] for t in self.datastore]
+        try:
+            pickle.dump(tweets_to_save, open(self.path, 'wb'))
+            print('saved cache to disk with %i tweets' % len(tweets_to_save))
+        except:
+            logging.error('unable to save cache, writing')
+            self._trim_cache(len(self.datastore))
 
 def main():
     import argparse
